9df52f9a20a2daea965c172c3c2ce7a93d25fd93
Ariful Islam <ariful.pust@gmail.com>
2017-11-24 22:51:58 +0600

    first commit

--- app/src/main/java/com/ariful/kotlinapp/MainActivity.kt
+++ app/src/main/java/com/ariful/kotlinapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.ariful.kotlinapp
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -10,36 +11,35 @@ import android.widget.Toast
 import butterknife.*
 
 class MainActivity : AppCompatActivity() {
-    var result:TextView?=null
-
     lateinit var unbinder:Unbinder
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
        // unbinder= ButterKnife.bind(this)
-        val add = findViewById<Button>(R.id.Add)
-        val first = findViewById<EditText>(R.id.first)
-        val second = findViewById<EditText>(R.id.second)
-        result = findViewById<TextView>(R.id.result)
-        add.setOnClickListener {
+        val simpleOperation = findViewById<Button>(R.id.simpleOperation)
+        val JavaInKotlin = findViewById<Button>(R.id.JavaInKotlin)
+        val pdfFile = findViewById<Button>(R.id.pdfFile)
 
-            var Result: Int?=0
-            Result=first.text.toString().trim().toInt()+second.text.toString().trim().toInt()
-            result!!.text=Result.toString()+" fdeswfew"
-          //  result.setText("\ndqwdeqw\n32r23")
-            Toast.makeText(applicationContext,"result "+Result,Toast.LENGTH_LONG).show()
-            Log.e("Result rr ",Result.toString()+"qq "+first.text.toString().trim().toInt())
-            Log.e("Result rr ",Result.toString()+"qq "+first.text.toString().trim().toInt())
-            Log.e("Result rr ",Result.toString()+"qq "+first.text.toString().trim().toInt())
-            Log.e("Result rr ",Result.toString()+"qq "+first.text.toString().trim().toInt())
-            Log.e("Result rr ",Result.toString()+"qq "+first.text.toString().trim().toInt())
+
+        simpleOperation.setOnClickListener {
+            val changePage = Intent(this, SimpleOperation::class.java)
+            startActivity(changePage)
         }
+        JavaInKotlin.setOnClickListener {
+            val changePage = Intent(this, ButterKnifeTest::class.java)
+            startActivity(changePage)
+        }
+        pdfFile.setOnClickListener {
+            val changePage = Intent(this, CreateAndViewPdfFile::class.java)
+            startActivity(changePage)
+        }
+
     }
 
 
     override fun onDestroy() {
         super.onDestroy()
-        unbinder.unbind()
+       // unbinder.unbind()
 
     }
 }
---
+++ app/src/main/java/com/ariful/kotlinapp/SimpleOperation.kt
@@ -0,0 +1,40 @@
+package com.ariful.kotlinapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import butterknife.Unbinder
+
+ class SimpleOperation : AppCompatActivity() {
+    var result: TextView?=null
+    lateinit var unbinder: Unbinder
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+        // unbinder= ButterKnife.bind(this)
+        val add = findViewById<Button>(R.id.Add)
+        val first = findViewById<EditText>(R.id.first)
+        val second = findViewById<EditText>(R.id.second)
+        result = findViewById<TextView>(R.id.result)
+        add.setOnClickListener {
+            var Result: Int?
+            Result=first.text.toString().trim().toInt()+second.text.toString().trim().toInt()
+            result!!.text=Result.toString()+" fdeswfew"
+            //  result.setText("\ndqwdeqw\n32r23")
+            Toast.makeText(applicationContext,"result "+Result, Toast.LENGTH_LONG).show()
+
+            Log.e("Result rr ",Result.toString()+"qq "+first.text.toString().trim().toInt())
+        }
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+//        unbinder.unbind()
+
+    }
+}
